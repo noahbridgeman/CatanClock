221c81150c2e98da46cec90cd7875fc9f64864ea
noahbridgeman <noah.bridgy@icloud.com>
2025-12-15 13:35:51 +0100

    deployment ready

--- CatanClock/app.py
+++ CatanClock/app.py
@@ -9,8 +9,7 @@ def create_app(test_config=None):
 
 
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'dev'),
     )
 
     if test_config is None:
